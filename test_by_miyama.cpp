2a1bb4ccfee260bdec76ad607c838f22d07ec620
kenji <kenji@kenji-Vostro-3350.(none)>
2012-06-07 16:48:27 +0900

    test commit 2 to test

--- test_by_miyama.cpp
+++ test_by_miyama.cpp
@@ -4,4 +4,5 @@ int main(){
   std::cerr <<"test by miyama" <<std::endl;
   std::cerr << "piyopiyo" << std::endl;
   std::cerr << "2012-06-07" << std::endl;
+	std::cerr << "hogehoge" << std::endl;
 }
